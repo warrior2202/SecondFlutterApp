18a9338d6e32c04b85732f68f970cb2144232991
Manan Shah <43475582+warrior2202@users.noreply.github.com>
2020-06-06 12:11:10 +0530

    added circle avater,card,listtitle,custom fonts,column,row,material icon

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,85 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.teal,
-          body: Row(
-//            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100,
-                height: double.infinity,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/testapp1.jpeg"),
               ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 100,
-                height: double.infinity,
-                color: Colors.blue,
+              Text(
+                "Manan Shah",
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: "Pacifico"),
               ),
 
+              Text(
+                "Flutter Developer",
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 18.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Card(
+
+
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: ListTile(
+                    leading: Icon(
+                        Icons.phone, size: 20.0, color: Colors.blueAccent),
+                    title: Text("9029318325"),
+
+
+                  ),
+                ),
+              ),
+              Card(
+
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+
+
+//                padding: EdgeInsets.all(17.0),
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(50),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+
+                  child: ListTile(
+
+                    leading: Icon(
+                        Icons.email, size: 20.0, color: Colors.blueAccent),
+                    title: Text("MananShah2019@gmail.com"),
+
+
+                  ),
+                ),
+              ),
             ],
-          )
-          
+          ),
         ),
       ),
     );
   }
 }
+
